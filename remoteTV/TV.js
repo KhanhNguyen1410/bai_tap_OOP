c7ff0cbba4531bc69227d1b72d36e05566a32095
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-06 18:03:22 +0700

    add remoteTV

---
+++ remoteTV/TV.js
@@ -0,0 +1,47 @@
+class TV {
+    constructor(channel, volume, status) {
+        this.channel = channel;
+        this.volume = volume;
+        this.status = status;
+    };
+
+    move(Remote) {
+        if (this.status){
+            Remote.Controls()
+            this.channel = Remote.code;
+        } else {
+            alert("plz turnOn TV");
+        }
+
+    };
+
+    Status() {
+        alert("current channel: " + this.channel + ", current volume: " + this.volume)
+    };
+
+    volumeUp() {
+        if (this.status) {
+            this.volume += 1;
+            alert(this.volume);
+        } else {
+            alert("plz turnOn TV")
+        }
+    }
+
+    volumeDown() {
+        if (this.status) {
+            this.volume -= 1;
+            alert(this.volume);
+        } else {
+            alert("plz turnOn TV")
+        }
+    }
+
+    turnOnTV() {
+        alert("TV on");
+    };
+
+    turnOffTV() {
+        alert("TV off");
+    }
+}
---
+++ remoteTV/remote.js
@@ -0,0 +1,26 @@
+class Remote{
+    constructor(code) {
+        this.code = code;
+    };
+    Controls(){
+        this.code = +prompt("enter your channel")
+    };
+    channel(TV){
+        if (!TV.status){
+            alert("plz turnOn TV");
+        } else {
+            TV.Status();
+        }
+
+    };
+    turnOnTV(TV){
+        TV.turnOnTV();
+        TV.status = true;
+    };
+    turnOffTV(TV){
+        TV.turnOffTV();
+        TV.status = false;
+    };
+
+
+}
